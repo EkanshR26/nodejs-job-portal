071c074b00dbc231f6f4da9dfee838ffb042db73
EkanshR26 <168906036+EkanshR26@users.noreply.github.com>
2024-05-05 17:28:33 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,26 +2,27 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
-//schema
+
+// Schema
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Name Is Require"],
+      required: [true, "Name is required"],
     },
     lastName: {
       type: String,
     },
     email: {
       type: String,
-      required: [true, " Email is Require"],
+      required: [true, "Email is required"],
       unique: true,
       validate: validator.isEmail,
     },
     password: {
       type: String,
-      required: [true, "password is require"],
-      minlength: [6, "Password length should be greater than 6 character"],
+      required: [true, "Password is required and should be at least 6 characters long"],
+      minlength: [6, "Password length should be greater than 6 characters"],
       select: true,
     },
     location: {
@@ -31,23 +32,24 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-// middlewares
-userSchema.pre("save", async function () {
-  if (!this.isModified) return;
+
+// Middleware to hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified()) return next(); // Call the function isModified()
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-//compare password
+// Compare password
 userSchema.methods.comparePassword = async function (userPassword) {
   const isMatch = await bcrypt.compare(userPassword, this.password);
   return isMatch;
 };
 
-//JSON WEBTOKEN
+// JSON Web Token
 userSchema.methods.createJWT = function () {
-  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
+
 export default mongoose.model("User", userSchema);
